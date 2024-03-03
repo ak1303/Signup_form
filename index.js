4e7fb56927e3623e56d4f4e335e6f7e32b5813cb
ak1303 <13abhi03@gmail.com>
2024-03-04 00:14:47 +0530

    code added

--- index.js
+++ index.js
@@ -3,40 +3,74 @@ let signup = {
     email:'',
     password:''
 };
-document.addEventListener('change',(e)=>{ //min 4 char, '@ 'and '.'
-    let correct_email=false;
-    let correct_password=false;
+function checkEmail(email){
+    let cond1 = email.length>3;
+    let cond2 = email.indexOf('@')!=-1;
+    let cond3 = email.indexOf('.')!=-1;
+    let error = document.querySelector('.invalid_email');
+    if(cond1 && cond2 && cond3){
+        error.classList.add('hide');
+    }
+    else  error.classList.remove('hide');
+}
+function checkPasword(password){
+    let error = document.querySelector('.invalid_password');
+    if(password.length<=8){
+        error.classList.remove('hide');
+    }else  error.classList.add('hide');
+}
+document.addEventListener('change',(e)=>{
     if(e.target.id=='email'){
         signup.email=e.target.value;
-        let cond1 = signup.email.length>3;
-        let cond2 = signup.email.includes('@');
-        let cond3 = signup.email.includes('.');
-        let error = document.querySelector('.invalid_email');
-        if(cond1 && cond2 && cond3){
-            error.classList.remove('hide');
-            correct_email=true;
-        }
-        else  error.classList.add('hide');
-
+        checkEmail(signup.email);
     }
     if(e.target.id=='password'){
         signup.password=e.target.value;
-        if(signup.password.length<=8){
-            let error = document.querySelector('.invalid_password');
-            error.classList.remove('hide');
-            correct_password=true;
-        }else  error.classList.add('hide');
+        checkPasword(signup.password);
     }
-    if(correct_email && correct_password){
-        let successMsg =document.querySelector('.success');
-        successMsg.classList.remove('hide');
-        successMsg.classList.add('success');
-    }
-    console.log(signup);
 })
+let successMsg =  document.querySelector('.success');
+document.addEventListener('input',(e)=>{ //min 4 char, '@ 'and '.'
+    let valid =true;
+    if(e.target.id=='password'){
+        let password=e.target.value;
+        let cond1 = signup.email.length>3;
+        let cond2 = signup.email.indexOf('@')!=-1;
+        let cond3 = signup.email.indexOf('.')!=-1;
+        let cond4 = password.length>8;
+        if(!cond1 || !cond2 || !cond3 || !cond4){
+            valid=false;
+        }else valid=true;
+    }
+    if(e.target.id=='email'){
+        let email=e.target.value;
+        let cond1 = email.length>3;
+        let cond2 = email.indexOf('@')!=-1;
+        let cond3 = email.indexOf('.')!=-1;
+        let cond4 = signup.password.length>8
+        if(!cond1 || !cond2 || !cond3 || !cond4){
+            valid=false;
+        }else valid=true;
+    }
+    if(valid){
+        let invEmail = document.querySelector('.invalid_email');
+        let invPass = document.querySelector('.invalid_password');
+        invEmail.classList.add('hide');
+        invPass.classList.add('hide');
+        successMsg.classList.remove('hide');
+    }
+    else successMsg.classList.add('hide');
+});
 
 let form = document.getElementById('form1');
-form.addEventListener('submit',()=>{
-    if(sig)
-    alert("hello");
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let response = window.confirm('Are you sure you want to submit form');
+    if(response){
+        successMsg.classList.add('hide');
+        signup.email='';
+        signup.password='';
+        alert("Successful signup!");
+        form.reset();   
+    }
 })
